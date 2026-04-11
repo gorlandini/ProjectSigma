55df68ca419efacd3749dbcc8f44cefb7393c0e3
Guilherme Orlandini <guilherme.orlandini@gmail.com>
2026-04-11 13:48:33 -0300

    Initial commit

---
+++ src/main/java/com/pay/projectsigma/ProjectSigmaApplication.java
@@ -0,0 +1,13 @@
+package com.pay.projectsigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSigmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSigmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pay/projectsigma/ProjectSigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.projectsigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSigmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
